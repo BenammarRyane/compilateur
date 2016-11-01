f0ccbc83896a8b18cbc2795d71544a9ce31ac8dc
BenammarRyane <ryane.benammar@gmail.com>
2016-11-01 20:09:12 +0100

    premier pas

---
+++ homework.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <ctype.h>
+int main(void){
+
+int i;
+int n;
+int tab[n] ;
+int isdigit(int *tab[i]);
+remplir(i,n,&tab[i]);
+ lecturetest(&tab[i]);
+}
+ void remplir(int i,int n,int *tab[n]){
+   printf("donnez la longueur e votre chaine");
+   scanf("%d",n);
+        for(i=0;i<n;i++){
+        printf("remplissez encore votre tableau");
+        scanf("%d",tab[i]);
+        }
+  } 
+ void lecturetest(int *tab[i]){
+         while(i<=n){
+         if(isdigit(tab[i])){
+         i++;   }
+         
+                    }
+         printf("ce que vous avez introduit est un nombre");
+         for(i=0;i<n;i++)
+         pritnf("votre nombre est %d",tab[0]); 
+
+        }
